c246b9cfec568333f6e3eb7616443df03bf35f65
harisankar <harisankar705@gmail.com>
2025-02-22 03:37:38 +0530

    VITE ISSUE SOLVED

--- src/utils/ImageUtils.ts
+++ src/utils/ImageUtils.ts
@@ -1,4 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 export const getImageURL = (
     imagePath: string | undefined,
     type: 'profile-pictures' | 'posts' | 'company-documents'|'company-logos',
